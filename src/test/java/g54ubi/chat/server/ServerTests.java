776749b5f5756a3001a1428b279fc6296862999a
Alex Neves <alexneves08@gmail.com>
2015-12-04 18:24:19 +0000

    Test class and JUnit dependency
    Added an initial test class and the JUnit dependency in the Maven
    pom.xml

---
+++ src/test/java/g54ubi/chat/server/ServerTests.java
@@ -0,0 +1,11 @@
+package g54ubi.chat.server;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServerTests {
+    @Test
+    public void Test_One() {
+        Assert.assertEquals(true, true);
+    }
+}
