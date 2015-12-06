a903e9cf845da1f5fc58eac88056ba6fd5c2620c
Alex Neves <alexneves08@gmail.com>
2015-12-06 19:39:29 +0000

    Connection tests implementation
    Implemented the test stubs in ConnectionTests

--- src/test/java/g54ubi/chat/server/ConnectionTests.java
+++ src/test/java/g54ubi/chat/server/ConnectionTests.java
@@ -1,16 +1,21 @@
 package g54ubi.chat.server;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public final class ConnectionTests {
+    private static final String IdentityCommand = "IDEN";
+
     private IChatClient mockChatClient;
     private IChatServer mockChatServer;
     private IMessageListener mockMessageListener;
@@ -25,8 +30,8 @@ public final class ConnectionTests {
 
         mockMessageListener = mock(IMessageListener.class);
 
-        doAnswer((i) -> {
-            messageReceivedListener = (IMessageReceivedListener) i.getArguments()[0];
+        doAnswer((invocation) -> {
+            messageReceivedListener = (IMessageReceivedListener) invocation.getArguments()[0];
             return "";
         }).when(mockMessageListener).registerMessageReceivedListener(any());
 
@@ -40,16 +45,51 @@ public final class ConnectionTests {
 
     @Test
     public void messageForConnection_SendsExpectedMessage() {
+        final String expectedMessage = "Expected Message";
 
+        connection.messageForConnection(expectedMessage);
+
+        assertChatClientReceivedMessage(expectedMessage);
     }
 
     @Test
     public void getUserName_BeforeIdenHasBeenCalled_ReturnsNull() {
+        final String userName = connection.getUserName();
+
+        assertThat(userName, is(nullValue()));
     }
 
     @Test
-    public void getUserName_AfterIdenHasBeenCalled_ReturnsExpectedUserName() {
+    public void getUserName_AfterIdenHasBeenCalledWithUserName_WithNoSpaces_ReturnsExpectedUserName() {
+        final String expectedUserNameWithoutSpace = "User1";
+
+        sendCommand(IdentityCommand, expectedUserNameWithoutSpace);
+
+        final String userName = connection.getUserName();
+
+        assertThat(userName, is(expectedUserNameWithoutSpace));
     }
 
+    @Ignore
+    public void getUserName_AfterIdenHasBeenCalledWithUserName_WithSpaces_ReturnsExpectedUserName() {
+        final String expectedUserNameWithSpace = "User 1";
 
+        sendCommand(IdentityCommand, expectedUserNameWithSpace);
+
+        final String userName = connection.getUserName();
+
+        assertThat(userName, is(expectedUserNameWithSpace));
+    }
+
+    private void assertChatClientReceivedMessage(final String expectedMessage) {
+        verify(mockChatClient).sendMessage(expectedMessage);
+    }
+
+    private void sendCommand(final String command) {
+        messageReceivedListener.onMessageReceived(command);
+    }
+
+    private void sendCommand(final String command, final String args) {
+        messageReceivedListener.onMessageReceived(command + " " + args);
+    }
 }
