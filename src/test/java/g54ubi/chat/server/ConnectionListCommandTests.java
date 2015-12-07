64bb177a64155fa013bca03a2a21b42c38b747d1
Alex Neves <alexneves08@gmail.com>
2015-12-07 16:48:04 +0000

    List Command Tests

---
+++ src/test/java/g54ubi/chat/server/ConnectionListCommandTests.java
@@ -0,0 +1,69 @@
+package g54ubi.chat.server;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
+public final class ConnectionListCommandTests extends ConnectionTestBase {
+    private ArrayList<String> userList;
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+
+        userList = new ArrayList<>();
+        when(mockChatServer.getUserList()).thenReturn(userList);
+    }
+
+    @Test
+    public void listCommand_WhenUnregistered_ClientReceivesErrorMessage() {
+        initialiseUnregisteredConnection();
+
+        sendCommand(LIST_COMMAND);
+
+        assertChatClientReceivedErrorMessage();
+    }
+
+    @Test
+    public void listCommand_WhenRegistered_ClientReceivesSuccessMessage() {
+        sendCommand(LIST_COMMAND);
+
+        assertChatClientReceivedSuccessMessage();
+    }
+
+    @Test
+    public void listCommand_WhenRegistered_AndNoClientsAreConnected_ClientReceivesExpectedUserList() {
+        final String expectedMessage = "OK ";
+
+        sendCommand(LIST_COMMAND);
+
+        assertChatClientReceivedExpectedMessage(expectedMessage);
+    }
+
+    @Test
+    public void listCommand_WhenRegistered_AndOneClientIsConnected_ClientReceivesExpectedUserList() {
+        userList.add("User1");
+
+        final String expectedMessage = "OK User1, ";
+
+        sendCommand(LIST_COMMAND);
+
+        assertChatClientReceivedExpectedMessage(expectedMessage);
+    }
+
+    @Test
+    public void listCommand_WhenRegistered_AndTwoClientsAreConnected_ClientReceivesExpectedUserList() {
+        userList.add("User1");
+        userList.add("User2");
+
+        final String expectedMessage = "OK User1, User2, ";
+
+        sendCommand(LIST_COMMAND);
+
+        assertChatClientReceivedExpectedMessage(expectedMessage);
+    }
+}
