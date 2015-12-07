6cda572c18811324bba17c6310cdcd33ab6961a5
Alex Neves <alexneves08@gmail.com>
2015-12-07 13:34:52 +0000

    Added missing comments
    Added missing comments from IMessageListener

--- src/main/java/g54ubi/chat/server/IMessageListener.java
+++ src/main/java/g54ubi/chat/server/IMessageListener.java
@@ -4,6 +4,13 @@ package g54ubi.chat.server;
  * Listens for incoming messages and triggers the provided call back when one is received.
  */
 public interface IMessageListener {
+    /**
+     * Start listening for messages. Potentially blocking.
+     */
     void listen();
+
+    /**
+     * Stop listening for messages.
+     */
     void stop();
 }
