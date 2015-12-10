856b0bc97d963b32d576b73ba9f55873ca862e0a
Alex Neves <alexneves08@gmail.com>
2015-12-10 14:08:13 +0000

    Gui Component Names
    In preparation for GUI testing I have labelled each component to allow
    component look ups.

--- src/main/java/g54ubi/chat/client/gui/ChatSessionGui.java
+++ src/main/java/g54ubi/chat/client/gui/ChatSessionGui.java
@@ -8,12 +8,14 @@ import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
+import static g54ubi.chat.client.gui.ChatSessionGuiComponents.*;
+
 public final class ChatSessionGui extends JFrame implements WindowListener {
     private static final int INITIAL_FRAME_WIDTH = 800;
     private static final int INITIAL_FRAME_HEIGHT = 800;
 
     private final IChatSessionFactory chatSessionFactory;
-    private final JTextArea chatTextWindow;
+    private final JTextArea chatMessagesWindow;
     private final JTextField messageInputTextField;
     private final JTextField userNameInputTextField;
     private final JTextField privateMessageRecipientInputTextField;
@@ -26,7 +28,7 @@ public final class ChatSessionGui extends JFrame implements WindowListener {
 
         addWindowListener(this);
 
-        this.chatTextWindow = createChatTextWindow();
+        this.chatMessagesWindow = createChatMessagesWindow();
         this.messageInputTextField = createMessageInputTextField();
         this.userNameInputTextField = createUserNameInputTextField();
         this.privateMessageRecipientInputTextField = createPrivateMessageRecipientInputTextField();
@@ -66,46 +68,59 @@ public final class ChatSessionGui extends JFrame implements WindowListener {
         final GridBagConstraints c = createConstraints(4, 0, 3);
         c.weighty = 1;
         c.fill = GridBagConstraints.BOTH;
-        panel.add(createChatTextWindowScrollPane(this.chatTextWindow), c);
+        panel.add(createChatTextWindowScrollPane(this.chatMessagesWindow), c);
 
         // ROW 6
         panel.add(messageInputTextField, createConstraints(5, 0, 3));
     }
 
-    private JTextArea createChatTextWindow() {
-        final JTextArea chatTextWindow = new JTextArea();
-        chatTextWindow.setLineWrap(true);
-        return chatTextWindow;
+    private JTextArea createChatMessagesWindow() {
+        final JTextArea chatMessageWindow = new JTextArea();
+
+        chatMessageWindow.setLineWrap(true);
+        chatMessageWindow.setName(TEXT_AREA_CHAT_MESSAGES);
+
+        return chatMessageWindow;
     }
 
     private JTextField createMessageInputTextField() {
         final JTextField messageInputTextField = new JTextField();
+
         messageInputTextField.setMaximumSize(new Dimension(Integer.MAX_VALUE, messageInputTextField.getPreferredSize().height));
+        messageInputTextField.setName(ChatSessionGuiComponents.TEXT_FIELD_MESSAGE);
+
         return messageInputTextField;
     }
 
     private JTextField createUserNameInputTextField() {
         final JTextField userNameInputTextField = new JTextField(10);
+
         userNameInputTextField.setMaximumSize(new Dimension(userNameInputTextField.getPreferredSize().width, userNameInputTextField.getPreferredSize().height));
+        userNameInputTextField.setName(ChatSessionGuiComponents.TEXT_FIELD_USER_NAME);
+
         return userNameInputTextField;
     }
 
     private JTextField createPrivateMessageRecipientInputTextField() {
         final JTextField privateMessageRecipientInputTextField = new JTextField();
+
         privateMessageRecipientInputTextField.setMaximumSize(new Dimension(Integer.MAX_VALUE, privateMessageRecipientInputTextField.getPreferredSize().height));
+        privateMessageRecipientInputTextField.setName(ChatSessionGuiComponents.TEXT_FIELD_PRIVATE_MESSAGE_RECIPIENT);
+
         return privateMessageRecipientInputTextField;
     }
 
     private JPanel createPanel() {
         final JPanel panel = new JPanel();
         panel.setLayout(new GridBagLayout());
+
         return panel;
     }
 
     private GridBagConstraints createConstraints(final int row, final int column) {
         final GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.HORIZONTAL;
 
+        constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridy = row;
         constraints.gridx = column;
         constraints.weightx = 1;
@@ -122,78 +137,111 @@ public final class ChatSessionGui extends JFrame implements WindowListener {
 
     private JButton createStartSessionButton() {
         final JButton startSessionButton = new JButton("Start Session");
+
+        startSessionButton.setName(ChatSessionGuiComponents.BUTTON_START_SESSION);
+
         startSessionButton.addActionListener(actionListener -> {
             endSession();
             chatSession = chatSessionFactory.create(this::onServerResponseReceived);
             chatSession.start();
         });
+
         return startSessionButton;
     }
 
     private JLabel createUserNameInputTextFieldLabel() {
         final JLabel userNameInputTextFieldLabel = new JLabel("User Name:");
+
         userNameInputTextFieldLabel.setLabelFor(userNameInputTextField);
+        userNameInputTextFieldLabel.setName(ChatSessionGuiComponents.LABEL_USER_NAME);
+
         return userNameInputTextFieldLabel;
     }
 
     private JButton createListCurrentUsersButton() {
         final JButton listCurrentUsersButton = new JButton("List Current Users");
+
         listCurrentUsersButton.addActionListener(actionListener -> chatSession.listCurrentUsers());
+        listCurrentUsersButton.setName(ChatSessionGuiComponents.BUTTON_LIST_CURRENT_USERS);
+
         return listCurrentUsersButton;
     }
 
     private JButton createShowSessionStatisticsButton() {
         final JButton showSessionStatisticsButton = new JButton("Show Session Statistics");
+
         showSessionStatisticsButton.addActionListener(actionListener -> chatSession.getSessionStatistics());
+        showSessionStatisticsButton.setName(ChatSessionGuiComponents.BUTTON_SHOW_STATISTICS);
+
         return showSessionStatisticsButton;
     }
 
     private JLabel createPrivateMessageRecipientInputTextFieldLabel() {
         final JLabel privateMessageRecipientInputTextFieldLabel = new JLabel("Private Message Recipient:");
+
         privateMessageRecipientInputTextFieldLabel.setLabelFor(privateMessageRecipientInputTextField);
+        privateMessageRecipientInputTextFieldLabel.setName(ChatSessionGuiComponents.LABEL_PRIVATE_MESSAGE_RECIPIENT);
+
         return privateMessageRecipientInputTextFieldLabel;
     }
 
     private JButton createSendPrivateMessageButton() {
         final JButton sendPrivateMessageButton = new JButton("Send Private Message");
+
+        sendPrivateMessageButton.setName(ChatSessionGuiComponents.BUTTON_SEND_PRIVATE_MESSAGE);
+
         sendPrivateMessageButton.addActionListener(actionListener -> {
             final String recipient = privateMessageRecipientInputTextField.getText();
             final String message = messageInputTextField.getText();
             chatSession.sendPrivateMessage(recipient, message);
             messageInputTextField.setText("");
         });
+
         return sendPrivateMessageButton;
     }
 
     private JButton createSetUserNameButton() {
         final JButton setUserNameButton = new JButton("Set User Name");
+
+        setUserNameButton.setName(ChatSessionGuiComponents.BUTTON_SET_USER_NAME);
+
         setUserNameButton.addActionListener(actionListener -> {
             final String userName = userNameInputTextField.getText();
             chatSession.setUserName(userName);
         });
+
         return setUserNameButton;
     }
 
     private JButton createBroadcastMessageButton() {
         final JButton broadcastMessageButton = new JButton("Broadcast Message");
+
+        broadcastMessageButton.setName(ChatSessionGuiComponents.BUTTON_BROADCAST_MESSAGE);
         broadcastMessageButton.addActionListener(actionListener ->
         {
             chatSession.broadcastMessage(messageInputTextField.getText());
             messageInputTextField.setText("");
         });
+
         return broadcastMessageButton;
     }
 
     private JButton createQuitButton() {
         final JButton quitButton = new JButton("Quit");
+
         quitButton.addActionListener(actionListener -> endSession());
+        quitButton.setName(ChatSessionGuiComponents.BUTTON_QUIT);
+
         return quitButton;
     }
 
-    private JScrollPane createChatTextWindowScrollPane(final JTextArea chatTextWindow) {
-        final JScrollPane scrollPane = new JScrollPane(chatTextWindow);
-        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        return scrollPane;
+    private JScrollPane createChatTextWindowScrollPane(final JTextArea chatMessagesWindow) {
+        final JScrollPane chatMessagesScrollPane = new JScrollPane(chatMessagesWindow);
+
+        chatMessagesScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        chatMessagesScrollPane.setName(ChatSessionGuiComponents.SCROLL_PANE_CHAT_MESSAGES);
+
+        return chatMessagesScrollPane;
     }
 
     public void start() {
@@ -201,7 +249,7 @@ public final class ChatSessionGui extends JFrame implements WindowListener {
     }
 
     private void onServerResponseReceived(final String response) {
-        chatTextWindow.append(response + System.lineSeparator());
+        chatMessagesWindow.append(response + System.lineSeparator());
     }
 
     private void endSession() {
---
+++ src/main/java/g54ubi/chat/client/gui/ChatSessionGuiComponents.java
@@ -0,0 +1,24 @@
+package g54ubi.chat.client.gui;
+
+public final class ChatSessionGuiComponents {
+    public final static String TEXT_AREA_CHAT_MESSAGES = "TEXT_AREA_CHAT_MESSAGES";
+
+    public static final String SCROLL_PANE_CHAT_MESSAGES = "SCROLL_PANE_CHAT_MESSAGES";
+
+    public static final String TEXT_FIELD_MESSAGE = "TEXT_FIELD_MESSAGE";
+    public static final String TEXT_FIELD_USER_NAME = "TEXT_FIELD_USER_NAME";
+    public static final String TEXT_FIELD_PRIVATE_MESSAGE_RECIPIENT = "TEXT_FIELD_PRIVATE_MESSAGE_RECIPIENT";
+
+    public static final String LABEL_USER_NAME = "LABEL_USER_NAME";
+    public static final String LABEL_PRIVATE_MESSAGE_RECIPIENT = "LABEL_PRIVATE_MESSAGE_RECIPIENT";
+
+    public static final String BUTTON_START_SESSION = "BUTTON_START_SESSION";
+    public static final String BUTTON_SET_USER_NAME = "BUTTON_SET_USER_NAME";
+    public static final String BUTTON_LIST_CURRENT_USERS = "BUTTON_LIST_CURRENT_USERS";
+    public static final String BUTTON_SHOW_STATISTICS = "BUTTON_SHOW_STATISTICS";
+    public static final String BUTTON_SEND_PRIVATE_MESSAGE = "BUTTON_SEND_PRIVATE_MESSAGE";
+
+    public static final String BUTTON_BROADCAST_MESSAGE = "BUTTON_BROADCAST_MESSAGE";
+    public static final String BUTTON_QUIT = "BUTTON_QUIT";
+
+}
